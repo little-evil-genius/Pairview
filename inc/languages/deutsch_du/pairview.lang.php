aada956d6df95e3926ffb035009b15f21c1e7fb8
Lara <75126217+little-evil-genius@users.noreply.github.com>
2023-05-31 15:32:59 +0200

    Create pairview.lang.php

---
+++ inc/languages/deutsch_du/pairview.lang.php
@@ -0,0 +1,75 @@
+<?php
+$l['pairview_main'] = "Pärchenübersicht";
+$l['pairview_main_desc'] = "Wer mit wem und mit welchem Beziehungstatus ist doch sehr unübersichtlich, deswegen haben wir im Board eine Pärchenübersicht installiert. In dieser Übersicht könnt ihr eure Charakter mit ihren Partner oder Partnerin in verschiedene Kategorien eintragen. Wie ihr sehen könnt haben wir eine Menge an verschiedenen Kategorien und ihr könnt munter die verschiedenen Stadien einer Beziehung durchlaufen. Charaktere können auch doppelt eingetragen.";
+$l['pairview_navigation_lists'] = "Listen";
+
+$l['pairview_add_nav'] = "Neue Beziehung eintragen";
+$l['pairview_add_desc'] = "Du hast dich verliebt? Oder einfach nur jemanden gefunden, dem du gerne die Klamotten vom Leib reißt? Schön! Eintragen kannst du das hier genauso frei und offen wie die Liebe selbst. Da du das jederzeit ändern kannst, geh bitte genauso verantwortungsbewusst damit um, wie mit Verhütungsmitteln. Aktualisiere dein Pair, wenn sich etwas ändert oder lösche es, wenn ihr euch trennt. Für den Fall von Herzschmerz haben wir eine Taschentuchbox bereitgestellt";
+$l['pairview_add_type'] = "Beziehungstyp";
+$l['pairview_add_type_select'] = "Beziehungstyp auswählen";
+$l['pairview_add_partner_one'] = "Eigener Charakter";
+$l['pairview_add_partner_two'] = "Partnercharakter";
+$l['pairview_add_pic_one'] = "Gif vom eigenen Charakter";
+$l['pairview_add_pic_two'] = "Gif vom Partner";
+$l['pairview_add_iconsize'] = "max. Dateigröße: {1}";
+$l['pairview_add_icondims'] = "zugelassene Größe: {1} Pixel";
+$l['pairview_add_iconsquare'] = "Icon muss quadratisch sein";
+
+$l['pairview_error_double'] = "Das Pärchen ist schon in dieser Kategorie eingetragen, bitte wähle eine andere Kategorie aus.";
+$l['pairview_error_category'] = "Es wurde keine Kategorie für das Pärchen ausgewählt, bitte wähle die entsprechende Kategorie aus.";
+$l['pairview_error_partner'] = "Es wurde kein Partner ausgewählt, bitte trage einen Partner ein.";
+$l['pairview_error_picOne_upload'] = "Die Icon-Datei für den eigenen Charakter fehlt, bitte wähle eine Datei aus um sie hochzuladen.";
+$l['pairview_error_picTwo_upload'] = "Die Icon-Datei für den Partner fehlt, bitte wähle eine Datei aus um sie hochzuladen.";
+$l['pairview_error_picOne_upload_size'] = "Die Icon-Datei für den eigenen Charakter ist größer als {1}, bitte wähle eine kleinere Datei aus.";
+$l['pairview_error_picTwo_upload_size'] = "Die Icon-Datei für den Partner ist größer als {1}, bitte wähle eine kleinere Datei aus.";
+$l['pairview_error_picOne_upload_size'] = "Die Icon-Datei für den eigenen Charakter ist größer als {1}, bitte wähle eine kleinere Datei aus.";
+$l['pairview_error_picOne_upload_file'] = "Die Icon-Datei für den eigenen Charakter besitzt eine ungültige Dateiendung ({1}), bitte wähle eine andere Datei aus.";
+$l['pairview_error_picTwo_upload_file'] = "Die Icon-Datei für den Partner besitzt eine ungültige Dateiendung ({1}), bitte wähle eine andere Datei aus.";
+$l['pairview_error_picOne_upload_iconsquare'] = "Die Icon-Datei für den eigenen Charakter ist nicht quadratisch, bitte wähle eine andere Datei aus.";
+$l['pairview_error_picTwo_upload_iconsquare'] = "Die Icon-Datei für den Partner besitzt ist nicht quadratisch, bitte wähle eine andere Datei aus.";
+$l['pairview_error_picOne_upload_icondims'] = "Die Icon-Datei für den eigenen Charakter entspricht nicht der geforderten Größe von {1}, bitte wähle eine andere Datei aus.";
+$l['pairview_error_picTwo_upload_icondims'] = "Die Icon-Datei für den Partner entspricht nicht der geforderten Größe von {1}, bitte wähle eine andere Datei aus.";
+$l['pairview_error_picOne_extern'] = "Der Bildlink für den eigenen Charakter fehlt, bitte füge in das entsprechende Feld ein Link ein.";
+$l['pairview_error_picTwo_extern'] = "Der Bildlink für den Partner fehlt, bitte füge in das entsprechende Feld ein Link ein.";
+$l['pairview_error_picOne_extern_ssl'] = "Der Bildlink für den eigenen Charakter ist nicht über eine HTTPS Verbindung eingebunden, bitte wähle einen anderen Link aus.";
+$l['pairview_error_picTwo_extern_ssl'] = "Der Bildlink für den Partner ist nicht über eine HTTPS Verbindung eingebunden, bitte wähle einen anderen Link aus.";
+
+$l['pairview_error_picOne_upload_edit'] = "Die Icon-Datei für den ersten Partner fehlt, bitte wähle eine Datei aus um sie hochzuladen.";
+$l['pairview_error_picTwo_upload_edit'] = "Die Icon-Datei für ersten zweiten Partner fehlt, bitte wähle eine Datei aus um sie hochzuladen.";
+$l['pairview_error_picOne_upload_size_edit'] = "Die Icon-Datei für den ersten Partner ist größer als {1}, bitte wähle eine kleinere Datei aus.";
+$l['pairview_error_picTwo_upload_size_edit'] = "Die Icon-Datei für ersten zweiten Partner ist größer als {1}, bitte wähle eine kleinere Datei aus.";
+$l['pairview_error_picOne_upload_size_edit'] = "Die Icon-Datei für den ersten Partner ist größer als {1}, bitte wähle eine kleinere Datei aus.";
+$l['pairview_error_picOne_upload_file_edit'] = "Die Icon-Datei für den ersten Partner besitzt eine ungültige Dateiendung ({1}), bitte wähle eine andere Datei aus.";
+$l['pairview_error_picTwo_upload_file_edit'] = "Die Icon-Datei für ersten zweiten Partner besitzt eine ungültige Dateiendung ({1}), bitte wähle eine andere Datei aus.";
+$l['pairview_error_picOne_upload_iconsquare_edit'] = "Die Icon-Datei für den ersten Partner ist nicht quadratisch, bitte wähle eine andere Datei aus.";
+$l['pairview_error_picTwo_upload_iconsquare_edit'] = "Die Icon-Datei für ersten zweiten Partner besitzt ist nicht quadratisch, bitte wähle eine andere Datei aus.";
+$l['pairview_error_picOne_upload_icondims_edit'] = "Die Icon-Datei für den ersten Partner entspricht nicht der geforderten Größe von {1}, bitte wähle eine andere Datei aus.";
+$l['pairview_error_picTwo_upload_icondims_edit'] = "Die Icon-Datei für ersten zweiten Partner entspricht nicht der geforderten Größe von {1}, bitte wähle eine andere Datei aus.";
+$l['pairview_error_picOne_extern_edit'] = "Der Bildlink für den ersten Partner fehlt, bitte füge in das entsprechende Feld ein Link ein.";
+$l['pairview_error_picTwo_extern_edit'] = "Der Bildlink für den zweiten Partner fehlt, bitte füge in das entsprechende Feld ein Link ein.";
+$l['pairview_error_picOne_extern_ssl_edit'] = "Der Bildlink für den ersten Partner ist nicht über eine HTTPS Verbindung eingebunden, bitte wähle einen anderen Link aus.";
+$l['pairview_error_picTwo_extern_ssl_edit'] = "Der Bildlink für den zweiten Partner ist nicht über eine HTTPS Verbindung eingebunden, bitte wähle einen anderen Link aus.";
+
+$l['pairview_options_delete'] = 'X';
+$l['pairview_options_edit'] = 'E';
+
+$l['pairview_edit'] = "Pärchen bearbeiten";
+$l['pairview_edit_type'] = "Beziehungstyp";
+$l['pairview_edit_partner_one'] = "erster Partner";
+$l['pairview_edit_partner_two'] = "zweiter Partner";
+$l['pairview_edit_button'] = "Änderungen speichern";
+
+$l['pairview_redirect_delete'] = "Du hast erfolgreich den Beziehungseintrag gelöscht! Du wirst nun auf die Pärchenübersicht zurückgeleitet.";
+$l['pairview_redirect_add'] = "Du hast erfolgreich das Pärchen eingetragen! Du wirst nun auf die Pärchenübersicht zurückgeleitet.";
+$l['pairview_redirect_edit_delete'] = "Das Pärchen ist schon in dieser Kategorie vorhanden. Der Eintrag, den du bearbeiten wolltest, wird hiermit für dich gelöscht! Du wirst nun auf die Pärchenübersicht zurückgeleitet.";
+$l['pairview_redirect_edit'] = "Du hast das Pärchen erfolgreich bearbeitet! Du wirst nun auf die Pärchenübersicht zurückgeleitet.";
+
+// MyALERTS
+$l['myalerts_pairview_add'] = '{1} kriegt dich nicht mehr aus dem Kopf und hat euch in die Kategorie <b>{3}</b> eingetragen.';
+$l['myalerts_setting_pairview_add'] = "Benachrichtigung erhalten, wenn man in die Pärchenübersicht eingetragen wird?";
+$l['myalerts_pairview_delete'] = '{1} hat die Sache zwischen euch beendet und den Eintrag in der Kategorie <b>{3}</b> aus der Pärchenübersicht gelöscht.';
+$l['myalerts_setting_pairview_delete'] = "Benachrichtigung erhalten, wenn Einträge in die Pärchenübersicht gelöscht werden?";
+$l['myalerts_pairview_edit'] = '{1} hat etwas an eurem Beziehungseintrag in der Pärchenübersicht verändert.';
+$l['myalerts_setting_pairview_edit'] = "Benachrichtigung erhalten, wenn die Icons der Einträge in die Pärchenübersicht bearbeitet werden?";
+
+?>
